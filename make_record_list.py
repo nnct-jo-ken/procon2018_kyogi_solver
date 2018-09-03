2849ffcdfb131c91c1e65aa03664b14eb1ad798b
t-iwabuchi <16403.nit@gmail.com>
2018-09-03 15:05:17 +0900

    set default dir

--- make_record_list.py
+++ make_record_list.py
@@ -5,7 +5,7 @@ import random
 
 #引数の設定
 parser = argparse.ArgumentParser()
-parser.add_argument('dir', type=str, help='Directory where records are saved')  #対局データが保存されているフォルダ
+parser.add_argument('dir', type=str, default="./record", help='Directory where records are saved')  #対局データが保存されているフォルダ
 parser.add_argument('--ratio', type=float, defaul=0.9, help="Proportion of records for train")  #訓練用データの割合
 args = parser.parse_args()
 
