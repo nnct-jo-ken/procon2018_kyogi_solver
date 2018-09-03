c5c085292e9299a2dc02070ea32e1c8f71a6a2eb
t-iwabuchi <16403.nit@gmail.com>
2018-09-03 15:04:06 +0900

    いったん綺麗に消す

--- utils/features.py
+++
@@ -1,6 +0,0 @@
-
-def make_features(position):
-    width, height, own_a1, own_a2, opponent_a1, opponent_a2, value, state = position
-    features = make_input_features(width, height,value, state)
-
-def make_input_features():
